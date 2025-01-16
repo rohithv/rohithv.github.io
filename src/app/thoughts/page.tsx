84d54bd6878e1fe50a77f586a735f396c71e3d01
Rohith Vishnumolakala <rohith.vishnu333@gmail.com>
2025-01-16 21:32:17 +0530

    add draft thoughts

--- src/app/thoughts/page.tsx
+++ src/app/thoughts/page.tsx
@@ -1,16 +1,52 @@
 'use client'
 import { motion } from 'framer-motion'
+import { title } from 'process'
 import { FaClock, FaTag } from 'react-icons/fa'
 
 export default function Thoughts() {
   const thoughts = [
     {
-      title: "Starting My Research Journey",
-      preview: "As I begin my masters program, I'm excited to share my experiences and insights...",
-      date: "2024-03-20",
+      title: "My Research Experience",
+      preview: "As my masters program is coming to an end, I'm excited to share my experiences and insights...",
+      date: "2025-01-16",
       tags: ["Research", "Academia"],
       status: 'draft'
     },
+    {
+        title: "Why I Left My High-Paying Job to Pursue a Master’s in India?",
+        preview: "I'm reflecting on my decision to leave my high-paying job for a masters in India...",
+        date: "2025-01-13",
+        tags: ["Career", "Academia"],
+        status: 'draft'
+    },
+    {
+        title: "Navigating the Transition: From Industry to Academia",
+        preview: "Transitioning from industry to academia can be a challenging yet rewarding journey...",
+        date: "2025-01-10",
+        tags: ["Career", "Academia"],
+        status: 'draft'
+    },
+    {
+        title: "The Intersection of Computer Science and Entrepreneurship",
+        preview: "The intersection of computer science and entrepreneurship offers endless possibilities, where technical expertise meets business innovation to create...",
+        date: "2025-01-06",
+        tags: ["Career", "Technology"],
+        status: 'draft'
+    },
+    {
+        title: "Why Collaboration is Key to Successful Software Development?",
+        preview: "Collaboration is the cornerstone of successful software development. It allows teams to share ideas, solve problems, and innovate...",
+        date: "2025-01-03",
+        tags: ["Technology", "Software Development"],
+        status: 'draft'
+    },
+    {
+        title: "Boosting Productivity with AI-Driven Tools",
+        preview: "Discover how AI-driven tools are revolutionizing software engineers’ productivity by...",
+        date: "2024-12-20",
+        tags: ["Technology", "Productivity"],
+        status: 'draft'
+    }
     // Add more thoughts here
   ]
 
